902863bb5b5c6da42ee8d899e91fb25ae1116eb3
Ryan <ryanssoliven@gmail.com>
2020-11-07 18:36:59 -0500

    2

---
+++ app/src/main/java/com/example/courtfinder/Court.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 2018 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.courtfinder;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.Ignore;
+import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
+
+/**
+ * Entity class that represents a word in the database
+ */
+
+@Entity(tableName = "court_table")
+public class Court {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    @NonNull
+    @ColumnInfo(name = "court")
+    private String mWord;
+
+    public Court(@NonNull String court) {
+        this.mWord = court;
+    }
+
+    /*
+    * This constructor is annotated using @Ignore, because Room expects only
+    * one constructor by default in an entity class.
+    */
+
+    @Ignore
+    public Court(int id, @NonNull String court) {
+        this.id = id;
+        this.mWord = court;
+    }
+
+    public String getWord() {
+        return this.mWord;
+    }
+
+    public int getId() {return id;}
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
---
+++ app/src/main/java/com/example/courtfinder/Settings.java
@@ -0,0 +1,20 @@
+package com.example.courtfinder;
+
+import android.os.Bundle;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class Settings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings);
+    }
+}
